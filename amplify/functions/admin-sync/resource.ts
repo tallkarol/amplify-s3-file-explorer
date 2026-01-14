9c3dcc5629a18b0951563862b856f330585d4863
tallkarol <karolzbuczek@gmail.com>
2026-01-14 12:20:52 +0100

    define resourceGroupName to prevent backend dev stack mishandeling

--- amplify/functions/admin-sync/resource.ts
+++ amplify/functions/admin-sync/resource.ts
@@ -3,4 +3,5 @@ import { defineFunction } from '@aws-amplify/backend';
 export const adminSync = defineFunction({
   name: 'admin-sync',
   timeoutSeconds: 30,
+  resourceGroupName: 'auth', // Assign to auth stack to avoid circular dependency
 });
